b3a594cb978acbadc1b21214f05f57ccd7d1769d
drert <54976369+drert@users.noreply.github.com>
2025-05-18 05:43:02 -0400

    yml update again to try fix

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,7 +17,6 @@ export default async function(eleventyConfig) {
 
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
-    eleventyConfig.addPassthroughCopy("src/CNAME");
 	eleventyConfig
 		.addPassthroughCopy({
 			"./public/": "/"
