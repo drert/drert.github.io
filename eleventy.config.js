180ff1588c87d3015fe14ad61cd6ba9862d707da
drert <54976369+drert@users.noreply.github.com>
2025-05-18 05:31:15 -0400

    CNAME passthrough, publish branch added

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,6 +17,7 @@ export default async function(eleventyConfig) {
 
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
+    eleventyConfig.addPassthroughCopy("src/CNAME");
 	eleventyConfig
 		.addPassthroughCopy({
 			"./public/": "/"
