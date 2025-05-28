727cde7b583f34730156cb121a76a89333d9a5bd
drert <54976369+drert@users.noreply.github.com>
2025-05-28 03:06:00 -0400

    take 5

--- eleventy.config.js
+++ eleventy.config.js
@@ -22,7 +22,8 @@ export default async function(eleventyConfig) {
 			"./public/": "/"
 		})
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
-
+    // eleventyConfig.addPassthroughCopy({})
+    
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
